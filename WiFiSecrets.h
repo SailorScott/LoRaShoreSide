edfdb9beff8d1e50f26559ed99ccef4bb4c91d6d
SailorScott <80358179+SailorScott@users.noreply.github.com>
2025-01-27 21:50:21 -0500

    Next gen. Add buffers for queing messages.

--- WiFiSecrets.h
+++ WiFiSecrets.h
@@ -1,3 +1,14 @@
-#define WIFISSID "SSID"
-#define WIFIPASSWRD "PASSWORD"
-#define DESTIATIONSERVER "https://www.YOUR-SERVER-THAT-CAN-CONSUME-POSTS.COM"
+
+// #define WIFISSID "PYC-Members 2.4GHz"
+// #define WIFIPASSWRD "Fair&Winds321"
+
+// PYC-Members 2.4GHz
+// PYC-Members 5GHz
+// Fair&Winds321
+// PYC- Guest 2.4GHz
+// PYC- Guest 5GHz
+// PYCguest1
+#define WIFISSID "CedarbookG"
+#define WIFIPASSWRD "5856700029"
+
+#define DESTIATIONSERVER "https://www.pultneyvilleyachtclub.org/api/BoatBouy/create.php"
